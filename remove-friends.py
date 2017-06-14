aa44042a7af703c58b5c77f685b9fe971c8a4266
Ruslan Isaev <rusikisaev@list.ru>
2017-06-14 13:20:35 +0300

    Add new messages

--- remove-friends.py
+++ remove-friends.py
@@ -4,13 +4,15 @@ import json
 config = {}
 execfile("config.py", config)
 
-user = raw_input("Type the user name here:")
+user = raw_input("Type your user name here:")
 
 twitter = Twitter(auth = OAuth(config["access_key"], config["access_secret"], config["consumer_key"], config["consumer_secret"]))
 results = twitter.friends.ids(screen_name = user)
 
 for n in range(0, len(results["ids"]), 100):
 	ids = results["ids"][n:n+100]
+
+print "Removing your friends, this may take a while..."
 
 for i in range(0,len(ids)):
     results = twitter.friendships.destroy(user_id = ids[i])
--- remove-tweets.py
+++ remove-tweets.py
@@ -3,9 +3,12 @@ from twitter import *
 config = {}
 execfile("config.py", config)
 
-user = raw_input("Type the user name here:")
+user = raw_input("Type your user name here:")
 
 twitter = Twitter(auth = OAuth(config["access_key"], config["access_secret"], config["consumer_key"], config["consumer_secret"]))
 results = twitter.statuses.user_timeline(screen_name = user)
+
+print "Removing your tweets, this may take a while..."
+
 for status in results:
 	remove_results = twitter.statuses.destroy(id = status["id"])
